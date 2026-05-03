883f76a98828215204c328cb2031e524920705f1
Aftab Alam <uxaftab@gmail.com>
2026-05-03 23:44:43 +0530

    initial commit

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,7 +1,18 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants";
+import axios from "axios";
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const rawBaseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+
+const baseQuery = async (args, api, extraOptions) => {
+  const result = await rawBaseQuery(args, api, extraOptions);
+
+  if (result.error) {
+    console.log("API Error:", result.error);
+  }
+
+  return result;
+};
 
 export const apiSlice = createApi({
   baseQuery,
