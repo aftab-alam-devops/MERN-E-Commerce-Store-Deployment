fb83a4bfcc11e457b6c5a4287c5189d9f4ed6c3a
Aftab Alam <uxaftab@gmail.com>
2026-05-04 01:56:46 +0530

    Update Home.jsx text to 'Sara Saman' and rebuild

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
         <>
           <div className="flex justify-between items-center">
             <h1 className="ml-[20rem] mt-[10rem] text-[3rem]">
-              All Products
+              Sara Saman
             </h1>
 
             <Link
